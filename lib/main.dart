91a1512286ce8e14fbdb483161837a03ce9d5563
asaf.patya <asaf.patya@plus500.com>
2023-03-27 23:33:08 +0300

    after second module

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/intro_screen.dart';
+
+void main(){
+  runApp(const GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey
+      ),
+      home: IntroScreen()
+    );
+  }
+}
